817ee670baa6358f5c880e7ca259f12d67492fb5
Cesar van Hardeveld <cees.van.hardeveld@hva.nl>
2022-11-06 13:05:59 +0100

    footer update

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,36 @@
 // JavaScript Document
-console.log("hi");
+// footer controll
+//first drop down
+var deButton1 = document.querySelector('footer section:nth-of-type(1) button');
+var deHeader1 = document.querySelector('footer section:nth-of-type(1) h2');
+var footerSectie1 = document.querySelector('footer section:nth-of-type(1)');
+
+deButton1.addEventListener('click', toggleFooterInfo1);
+deHeader1.addEventListener('click', toggleFooterInfo1);
+
+function toggleFooterInfo1() {
+	footerSectie1.classList.toggle('open');
+}
+//second dropdown
+var deButton2 = document.querySelector('footer section:nth-of-type(2) button');
+var deHeader2 = document.querySelector('footer section:nth-of-type(2) h2');
+var footerSectie2 = document.querySelector('footer section:nth-of-type(2)');
+
+deButton2.addEventListener('click', toggleFooterInfo2);
+deHeader2.addEventListener('click', toggleFooterInfo2);
+
+function toggleFooterInfo2() {
+	footerSectie2.classList.toggle('open');
+}
+
+//second dropdown
+var deButton2 = document.querySelector('footer section:nth-of-type(2) button');
+var deHeader2 = document.querySelector('footer section:nth-of-type(2) h2');
+var footerSectie2 = document.querySelector('footer section:nth-of-type(2)');
+
+deButton2.addEventListener('click', toggleFooterInfo2);
+deHeader2.addEventListener('click', toggleFooterInfo2);
+
+function toggleFooterInfo2() {
+	footerSectie2.classList.toggle('open');
+}
